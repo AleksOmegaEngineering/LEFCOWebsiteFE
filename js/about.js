f38c0abee8e8b323d5c3bb615a82c45456451bda
Ashar Alvany <ashzee10000@gmail.com>
2023-01-17 23:17:43 -0600

    about page update

---
+++ js/about.js
@@ -0,0 +1,23 @@
+let ourOrgContainer = document.querySelector(".ourOrgContainer");
+let logoMissionStmnt = document.querySelector(".logoMissionStmnt");
+let aboutHeader = document.querySelector(".aboutHeader");
+let aboutHeaderTray = document.querySelector(".aboutHeaderTray");
+
+
+window.onload = (event) =>{
+    logoMissionStmnt.style.opacity = "1";
+    logoMissionStmnt.style.transform = "translateY(0vh)";
+}
+aboutHeader.addEventListener("mouseover", () =>{
+    aboutHeader.style.background = "white";
+    aboutHeaderTray.style.visibility = "visible";
+    aboutHeaderTray.style.opacity = "1";
+    aboutHeaderTray.style.transform= "translateY(100%)";
+
+});
+aboutHeader.addEventListener("mouseleave", () =>{
+    aboutHeader.style.background = "transparent";
+    aboutHeaderTray.style.opacity = "0";
+    aboutHeaderTray.style.transform= "translateY(0%)";
+    aboutHeaderTray.style.visibility = "hidden";
+});
