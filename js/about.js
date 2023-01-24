27eb240a5c81d000fe9b5e25db162ec91ac3c022
Ashar Alvany <ashzee10000@gmail.com>
2023-01-24 16:16:04 -0600

    Finished about us

--- js/about.js
+++ js/about.js
@@ -2,12 +2,11 @@ let ourOrgContainer = document.querySelector(".ourOrgContainer");
 let logoMissionStmnt = document.querySelector(".logoMissionStmnt");
 let aboutHeader = document.querySelector(".aboutHeader");
 let aboutHeaderTray = document.querySelector(".aboutHeaderTray");
+let scrollDownButton = document.getElementById("buttonTarget");
+let mainContent = document.querySelector(".mainContent");
+let row2 = document.querySelector(".row2");
+let intersecting = true;
 
-
-window.onload = (event) =>{
-    logoMissionStmnt.style.opacity = "1";
-    logoMissionStmnt.style.transform = "translateY(0vh)";
-}
 aboutHeader.addEventListener("mouseover", () =>{
     aboutHeader.style.background = "white";
     aboutHeaderTray.style.visibility = "visible";
@@ -21,3 +20,36 @@ aboutHeader.addEventListener("mouseleave", () =>{
     aboutHeaderTray.style.transform= "translateY(0%)";
     aboutHeaderTray.style.visibility = "hidden";
 });
+scrollDownButton.addEventListener("click", () => {
+    if(intersecting){
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        })
+    }
+    else{
+        window.scrollTo({
+            top: document.body.scrollHeight,
+            behavior: "smooth"
+        })
+    }
+});
+function updateArrow(bool) {
+    if(!bool){
+        scrollDownButton.innerHTML = "<i class=\"fa-solid fa-angle-down\"></i>"
+    }
+    else{
+        scrollDownButton.innerHTML = "<i class=\"fa-solid fa-angle-up\"></i>"
+    }
+}
+let options = {
+    threshold: [0, 1.0]
+}
+let callback = (entries, observer) => {
+    entries.forEach((entry) =>{
+        intersecting = entry.isIntersecting
+        updateArrow(intersecting);
+    });
+}
+let observer = new IntersectionObserver(callback, options);
+observer.observe(row2);
